aa9ac7f43f0c9110e6e5704bb9f416a8010147f5
Shane Reynolds <shane.k.reynolds@gmail.com>
2017-03-10 09:40:06 +1100

    Add a script for question 4 part C

---
+++ tutorial_1/tutorial_1_Q4c.m
@@ -0,0 +1,40 @@
+%%%%%%%%%%%%%%%%%%%%%%%%% 4 Periodic Waveforms %%%%%%%%%%%%%%%%%%%%%%%%%
+clear; clc;
+
+% fundamental frequency
+f0 = 25;
+Tmax = 1/f0;
+
+% sampling frequency
+fs = 20000;
+dur = 2*Tmax;
+t = 0:1/fs:dur;
+
+% Set up the vector of integers for the number of cosines
+N1 = [1:5]; % 5 cosines
+N2 = [1:10]; % 10 cosines
+N3 = [1:25]; % 25 cosines
+
+% Create the frequency and Zk vectors for N = 5
+f1 = N1*f0;
+Z1 = (j*(-1).^N1)./(2*pi*N1);
+x1 = sumcos(f1,Z1,fs,dur); % Create plot for the N = 5
+subplot(3,1,1)
+plot(t,x1)
+title('5 Cosines')
+
+% Create the frequency and Zk vectors for N = 10
+f2 = N2*f0;
+Z2 = (j*(-1).^N2)./(2*pi*N2);
+x2 = sumcos(f2,Z2,fs,dur); % Create plot for the N = 10
+subplot(3,1,2)
+plot(t,x2)
+title('10 Cosines')
+
+% Create the frequency and Zk vectors for N = 25
+f3 = N3*f0;
+Z3 = (j*(-1).^N3)./(2*pi*N3);
+x3 = sumcos(f3,Z3,fs,dur); % Create plot for the N = 25
+subplot(3,1,3)
+plot(t,x3)
+title('25 Cosines')
