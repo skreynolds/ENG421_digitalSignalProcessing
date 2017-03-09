99feed9b715da996c763f54a092dbe8337bb0433
Shane Reynolds <shane.k.reynolds@gmail.com>
2017-03-09 15:39:27 +1100

    Add script for tutorial question 1

---
+++ tutorial_1/tutorial_1_Q1.m
@@ -0,0 +1,50 @@
+clear; clc;
+%%%%%%%%%%%%%%%%%% 2.1 Complex Numbers %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% (a) We can define complex numbers z1 and z2 and plot them with zvect
+% and print them with zprint
+
+z1 = -1 +j*0.3;
+z2 = 0.8 + j*0.7;
+
+figure(1)
+title('Question 2.1 (a) Plot')
+zvect([z1 z2])
+zprint([z1 z2])
+
+% (b) Find the conjugate and inverse for z1 and z2
+
+z1_inv = 1/z1;
+z2_inv = 1/z2;
+
+z1_conj = conj(z1);
+z2_conj = conj(z2);
+
+figure(2)
+title('Question 2.1 (b) Plot')
+zvect([z1_inv z2_inv z1_conj z2_conj])
+zprint([z1_inv z2_inv z1_conj z2_conj])
+
+
+% (c) Compute z1 + z2 and plot. Use zcat to show the sum as vectors
+% head to tail and use zprint to display the result numerically.
+
+z_add = z1 + z2;
+
+figure(3)
+title('Question 2.1 (c) Plot')
+zvect([z_add])
+hold on
+zcat([z1 z2])
+zprint([z1 z2])
+
+% (d) Compute z1.z2 and z1/z2 and plot. Use zvect plot function to show
+% how the angles of z1 and z2 determine
+
+z_mult = z1*z2;
+z_div = z1/z2;
+
+figure(4)
+title('Question 2.1 (d) Plot')
+zvect([z_mult z_div])
+zprint([z_mult z_div])
