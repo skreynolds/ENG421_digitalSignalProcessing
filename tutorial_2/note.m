9aa10fb16514b0fb66c9148003cedb296b6ffa67
Shane Reynolds <shane.k.reynolds@gmail.com>
2017-03-21 15:08:52 +1100

    Add a function which takes a key number from a keyboard and a duration and creates a discrete time sinusoidal signal (via sampling) and outputs the signal ready for D-to-A conversion

---
+++ tutorial_2/note.m
@@ -0,0 +1,16 @@
+function tone = note(keynum,dur)
+% NOTE Produce a sinusoidal waveform corresponding to a given
+% piano key number
+
+% usage: tone = note(keynum, dur)
+
+% tone = the output sinusoidal waveform 
+% keynum = the piano keyboard number of the desired note
+% dur = the duration (in seconds) of the output note
+    
+    fs = 11025;
+    tt = 0:(1/fs):dur;
+    freq = 440*2^((keynum-49)/12);
+    tone = 100*cos(2*pi*freq*tt);
+    
+end
