4bbaeacfbbec0299d345f51965518d9df95a9cf1
Shane Reynolds <Shane Reynolds>
2017-04-08 10:11:06 +0930

    Add a script which create an FIR filter - three opint averager

---
+++ tutorial_4/threePointAverager_2.m
@@ -0,0 +1,23 @@
+%%%%%%%%% 2.1 Frequency Response of the Three-Point Averager %%%%%%%%%%%
+
+% Clear the workspace and any stored variables
+clear; clc;
+
+% Initialise parameters that will be used for the averager
+ww = linspace(-pi,pi,400);
+
+% Implementation of of the frequency response function
+% (This is the same as the transfer function - different noteation?)
+H = ( (2*cos(ww) + 1)/3 ).* exp(-j*ww);
+
+% Plot the Magnitude of the frequency response
+subplot(2,1,1)
+plot(ww,abs(H))
+xlabel('frequency (rad/sec)')
+ylabel('magnitude |H|')
+
+% Plot the Phase Angle Shift of the frequency response
+subplot(2,1,2)
+plot(ww,angle(H))
+xlabel('frequency (rad/sec)')
+ylabel('phase shift (rad)')
