d1020a6a66dc7b8e87d434659ef5e0e2081daadf
Shane Reynolds <shane.k.reynolds@gmail.com>
2017-05-06 12:26:48 +0930

    Add a script which will run a first differencer filter over some input signal.

---
+++ tutorial_4/firstDifferenceFilter_3_2.m
@@ -0,0 +1,33 @@
+%%%%%%%%%%%%% 3.3 Linearity of the Filter %%%%%%%%%%%%%%%%%%
+
+% Clear the worksapce and any stored variables
+clear; clc;
+
+L = 50;
+
+% Create a vector which will be used to create samples
+nn = 0:(L-1);
+
+% Initialise the parameters used to define the input signal
+A = 7;
+ph = pi/3;
+ww = 0.125*pi;
+
+% Take discrete samples of the cosine
+xx = A*cos(ww*nn + ph);
+
+% Create a linearly scaled version of xx
+xa = 2*xx;
+
+% Create the filter and the associated response to teh input signal
+bb = [5 -5];
+ya = firfilt(bb,xa);
+
+% Create plots to compare the inpust signal with the output signal
+subplot(2,1,1)
+plot(nn,xa)
+xlabel('sample number')
+
+subplot(2,1,2)
+plot(nn,ya(1:50))
+xlabel('sample number')
